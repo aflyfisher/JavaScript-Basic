30831d4583ffca8104d916386e9164f9ae0360a7
aflyfisher <XXXXXX.com>
2018-07-05 11:12:02 +0800

    eslint修改

--- chapter4/01.js
+++ chapter4/01.js
@@ -1,24 +1,24 @@
 var s = {
-    name:"张三"
+    name:'张三'
 };
 var k = s;
 k.age = 23;
 console.log(s.age);
 // 如果引用类型的变量s保存的是值，那么k保存的也是值，两者相互不影响
-//当为对象k添加属性age的时候，访问对象s的属性age，应该是undefined，但结果是23，这说明当改变k的时候影响了s，只能说明
-//访问s和k的时候，是同一个对象，所以，也就论证了笔记中的说法----引用类型变量保存的是地址（指针）
+// 当为对象k添加属性age的时候，访问对象s的属性age，应该是undefined，但结果是23，这说明当改变k的时候影响了s，只能说明
+// 访问s和k的时候，是同一个对象，所以，也就论证了笔记中的说法----引用类型变量保存的是地址（指针）
 /* ---------------------------------------------------------------------------------------------- */
 // 检测变量的值的类型
-console.log( typeof null);//object
-console.log(typeof {});//object
+console.log( typeof null);// object
+console.log(typeof {}); // object
 /* -------------------------------------------作用域------------------------------------------------ */
-var color = "blue";
+var color = 'blue';
 function changeColor() {
-    if (color === "blue") {
-        color = "red";
+    if (color === 'blue') {
+        color = 'red';
     } else {
-        color = "blue";
+        color = 'blue';
     }
 }
 changeColor();
-console.log("Color is now " + color);//red
+console.log('Color is now ' + color);// red撒打算
